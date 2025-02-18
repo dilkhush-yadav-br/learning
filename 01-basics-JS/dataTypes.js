6a725c701e233b46ffcec5ff82ea27d789ecb6f2
dilyadav1 <dilkhush.yadav@publicissapient.com>
2025-02-18 22:04:00 +0530

    conversion opeartions

---
+++ 01-basics-JS/dataTypes.js
@@ -0,0 +1,25 @@
+/* Data types:-
+1. Number
+2. Bigint
+3. String
+4. Boolean
+5. null
+6. undefined
+7. symbol
+8. Object
+*/
+
+// TODO: undertand each datatypes with examples
+let myName="Dilkhush"
+let age = 24
+let IsSingle = true;
+let haveBabies=null; // output = object
+let location =undefined  // output = undefined
+let obj ={"Name":"Dilkhush"}
+
+console.log(typeof myName)
+console.log(typeof age)
+console.log(typeof IsSingle)
+console.log(typeof haveBabies)
+console.log(typeof location)
+console.log(typeof obj)
---
+++ 01-basics-JS/variables.js
@@ -0,0 +1,13 @@
+const accountId = 123
+let accountName= "Dilkhush"
+var accountPass = 1234
+accCity = "Banglore"
+let accountState ;
+
+// accountId = 453
+/*
+var has block scope and functional scope issue , so let was introduced later
+sope means simple :{  inside this is scope  }
+*/
+console.log(accountId)
+console.table([accountId,accountName,accountPass,accountState,accCity])
