08a5d3a30f18bff0c750b138bedb632749203de5
dilyadav1 <dilkhush.yadav@publicissapient.com>
2025-02-18 23:00:48 +0530

    datatypes summary

---
+++ 01-basics-JS/comparison.js
@@ -0,0 +1,22 @@
+console.log("02" >1); //true
+console.log(2 >1);    //true
+
+// null
+console.log("NULL:-")   
+console.log(null > 0);  //false
+console.log(null < 0);  //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+// undefined
+console.log("UNDEFINED:-")
+console.log(undefined > 0);  //false
+console.log(undefined < 0);  //false
+console.log(undefined == 0); //false
+console.log(undefined >= 0); //false
+
+//  strict check ===
+console.log("Strict check === ")
+console.log("02" === 2); //false
+console.log(2 === 2);    //true
+
--- 01-basics-JS/dataTypes.js
+++ 01-basics-JS/dataTypes.js
@@ -15,11 +15,25 @@ let age = 24
 let IsSingle = true;
 let haveBabies=null; // output = object
 let location =undefined  // output = undefined
-let obj ={"Name":"Dilkhush"}
+let StuObj = {
+    "Name":"Dilkhush",
+    "usn":23,
+    "Age":12
+}
+const bigInt = 123657898765456789n
+const bigNum=BigInt(876543456789876544567)
+let id = Symbol('123')
+let HerosArr = ["Shaktiman","IronMan","Captain America","Ninja Hattori"]
+
 
 console.log(typeof myName)
 console.log(typeof age)
 console.log(typeof IsSingle)
 console.log(typeof haveBabies)
 console.log(typeof location)
-console.log(typeof obj)
+console.log(typeof StuObj)
+console.log(typeof bigInt)
+console.log(typeof bigNum)
+console.log(typeof id)
+console.log(typeof HerosArr) // this is array but it JS it return as Object
+console.log(Array.isArray(HerosArr));  // This will return true : To specifically check whether a variable is an array
